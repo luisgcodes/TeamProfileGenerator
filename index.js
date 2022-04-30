a0a0b8ec369fbf53aa2601b48fb213b9b2ba2c99
luisgcodes <lcienfuegos95@gmail.com>
2022-04-30 15:02:54 -0700

    created constVariables

---
+++ index.js
@@ -0,0 +1,8 @@
+// Const Variables 
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateHtml = require("./src/generateHtml")
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
