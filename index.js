8c8a2d8b3697b3965090b1762f6bede3accb2efe
luisgcodes <lcienfuegos95@gmail.com>
2022-04-30 15:18:45 -0700

    creating prompts

--- index.js
+++ index.js
@@ -6,3 +6,59 @@ const generateHtml = require("./src/generateHtml")
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+
+// Prompts
+var employeePrompts = [];
+
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter employee's name:",
+      },
+
+      {
+        type: "number",
+        name: "id",
+        message: "Enter employee's ID:",
+      },
+
+      {
+        type: "input",
+        name: "email",
+        message: "Enter employee's email:",
+        
+      },
+
+      {
+        type: "list",
+        name: "role",
+        message: "Select Employee's role from the list below",
+        choices: ["Manager", "Engineer", "Intern"],
+       
+      },
+
+      {
+        type: "number",
+        name: "officeNumber",
+        message: "Enter Manager's office number",
+        when: (answers) => answers.role === 'Manager'
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter Engineer's GitHub username",
+        when: (answers) => answers.role === 'Engineer'
+      },
+      {
+        type: "input",
+        name: "school",
+        message:
+          "Enter the name of the school this Intern is currently attending",
+          when: (answers) => answers.role === 'Intern',
+      },
+      
+    ])
